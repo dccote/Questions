5ca1ef30750ff05534b48ae40c404305044a9b29
Daniel Cote <dccote@novajo.ca>
2018-11-21 01:04:40 -0500

    More.

--- site/generatePDF.sh
+++ site/generatePDF.sh
@@ -1,6 +1,6 @@
-printf '# 001-Ondes\n\n'  > 001*/Q001.md
-cat 001/Q001-* >> 001/Q001.md
-printf '# 002-Optique Géometrique\n\n'  > 002/Q002.md
-cat 002/Q002-* >> Q002.md
+printf '# 001-Ondes\n\n'  > "001-Ondes/Q001.md"
+cat "001-Ondes/Q001-*" >> "001-Ondes/Q001.md"
+printf '# 002-Optique\ Géométrique\n\n'  > "002-Optique\ Géométrique/Q002.md"
+cat "002-Optique\ Géométrique/Q002-*" >> "Q002.md"
 printf '[TOC]\n\n'  > Questions.md                             
-cat 001/Q001.md 002/Q002.md > Questions.md
+cat "001-Ondes/Q001.md" "002-Optique\ Géométrique/Q002.md" > Questions.md
