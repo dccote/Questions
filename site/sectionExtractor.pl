f2625348c4fd1685de824beceff3c50d2b078d2f
Daniel Côté <dccote@novajo.ca>
2019-01-14 00:29:29 -0500

    NOuvelles questions et corrections.
    Ajout d'un search dans le finder pour tester: savedSearch est un plist.

--- site/sectionExtractor.pl
+++ site/sectionExtractor.pl
@@ -1,28 +1,41 @@
-$section = 0;
-$level = 0;
-$titles = ();
-$levels = ();
+
 @lines = <STDIN>;
+print sectionsFromLines(@lines);
 
-@text = ();
-@texts = ();
-while ($line = shift @lines) {
-	chomp($line);
 
-	if ($line =~ /^(#+)\s+(.+)$/) {
-		$header = $1;
-		$title = $2;
-		$level = length($header);
-		print "Level $level: $title\n\n";
-		push(@titles, $title);
-		push(@levels, $level);
-		push(@texts, @text); # previous section
-		print @text,"\n";
-		@text = ();
+
+# print join " ", @levels, "\n";
+# print join " ", @titles, "\n";
+# print join " ", @texts[3];
+
+
+sub sectionsFromLines {
+	my (@lines) = @_;
+
+	$section = 0;
+	$level = 0;
+	$titles = ();
+	$levels = ();
+
+	@text = ();
+	@texts = ();
+	while ($line = shift @lines) {
+		chomp($line);
+
+		if ($line =~ /^(#+)\s+(.+)$/) {
+			$header = $1;
+			$title = $2;
+			$level = length($header);
+			# print "Level $level: $title\n\n";
+			push(@titles, $title);
+			push(@levels, $level);
+			push(@texts, @text); # previous section
+			print @text,"\n";
+			@text = ();
+		}
+		push(@text, $line);
 	}
-	push(@text, $line);
-}
 
-print join " ", @levels, "\n";
-print join " ", @titles, "\n";
-print join " ", @texts[3];
+  	return (@levels, @titles, @texts);
+ }
+
