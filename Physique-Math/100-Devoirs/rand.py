5cde4a2e468f815158c6c31bd577e65b6671f7c3
Daniel Côté <dccote@cervo.ulaval.ca>
2019-10-26 16:30:17 -0400

    Encore fichiers redondants effacés

--- Physique-Math/100-Devoirs/rand.py
+++
@@ -1,9 +0,0 @@
-import numpy
-
-# numbers = numpy.random.normal(loc=10.16, scale=2.54, size=1000)
-# for n in numbers:
-# 	print(n)
-    
-numbers = numpy.random.normal(loc=4, scale=1, size=1000)
-for n in numbers:
-	print(n)
